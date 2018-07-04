5b57bb17f7c6ebbe1b5a8cee431cf4f6ace71782
1234567890 <phantiensy195@gmail.com>
2018-07-04 13:41:31 +0700

    thuc hanh arraylist don gian

--- src/main/java/trienKhaiLopListDonGian/MyList.java
+++ src/main/java/trienKhaiLopListDonGian/MyList.java
@@ -2,13 +2,12 @@ package trienKhaiLopListDonGian;
 
 import java.util.Arrays;
 
-public class MyList<I extends Number> {
+public class MyList<E> {
     private int size = 0;
     private int DEFAULT_CAPACITY = 10;
-    private Object elements[];
+    private Object elements[] = new Object[DEFAULT_CAPACITY];
 
     public MyList() {
-        elements = new Object[DEFAULT_CAPACITY];
     }
 
     private void ensureCapa() {
@@ -16,17 +15,21 @@ public class MyList<I extends Number> {
         elements = Arrays.copyOf(elements, newSize);
     }
 
-    public void add(Object object) {
+    public int getSize() {
+        return size;
+    }
+
+    public void add(E e) {
         if (size == elements.length) {
             ensureCapa();
         }
-        elements[size++] = object;
+        elements[size++] = e;
     }
 
-    public Object get(int i) {
+    public E get(int i) {
         if (i>= size || i <0) {
             throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
         }
-        return (Object) elements[i];
+        return (E) elements[i];
     }
 }
--- src/test/java/trienKhaiLopListDonGian/MyListTest.java
+++ src/test/java/trienKhaiLopListDonGian/MyListTest.java
@@ -13,8 +13,9 @@ public class MyListTest {
         System.out.println("element 1: "+listInteger.get(1));
         System.out.println("element 2: "+listInteger.get(2));
         System.out.println("element 3: "+listInteger.get(3));
+        System.out.println(listInteger.getSize());
 
-        listInteger.get(-1);
-        System.out.println("element -1: " + listInteger.get(-1));
+//        listInteger.get(-1);
+//        System.out.println("element -1: " + listInteger.get(-1));
     }
 }
