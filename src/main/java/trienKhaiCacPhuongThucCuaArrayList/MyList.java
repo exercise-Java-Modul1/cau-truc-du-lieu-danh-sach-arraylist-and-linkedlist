36542f464ec5f37ec0289d192006bfb7b3fe7959
1234567890 <phantiensy195@gmail.com>
2018-07-04 18:01:37 +0700

    bai tap tao MyListTest

---
+++ src/main/java/trienKhaiCacPhuongThucCuaArrayList/MyList.java
@@ -0,0 +1,59 @@
+package trienKhaiCacPhuongThucCuaArrayList;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[] = new Object[DEFAULT_CAPACITY];
+
+    public MyList() {
+
+    }
+
+
+    public void add( E element) {
+        elements[size] = element;
+        size++;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void getlist(int index) {
+        for (int i = 0; i < getSize(); i++) {
+            if (index == i) {
+                System.out.println(elements[i]);
+            }
+        }
+    }
+
+    public void remove(int index) {
+        for (int i = 0; i < getSize(); i++) {
+            if (index == i) {
+                for (int j = i; j < getSize(); j++) {
+                    elements[j] = elements[j + 1];
+                }
+                size--;
+            }
+        }
+    }
+
+    public void clear() {
+        for (int i = getSize(); i > 0; i--) {
+            for (int j = 0; j < getSize(); j++) {
+                elements[j] = elements[j + 1];
+            }
+            size--;
+        }
+    }
+
+    public void coverNumber(int index, int number) {
+        for (int i = 0; i < getSize(); i++) {
+            if (index == i) {
+                elements[i] = number;
+            }
+        }
+    }
+
+
+}
---
+++ src/test/java/trienKhaiCacPhuongThucCuaArrayList/MyListTest.java
@@ -0,0 +1,34 @@
+package trienKhaiCacPhuongThucCuaArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<Integer>();
+        list.add(5);
+        list.add(2);
+        list.add(6);
+        list.add(9);
+        list.add(7);
+        list.add(20);
+        list.add(15);
+        for (int i = 0; i < list.getSize(); i++) {
+            list.getlist(i);
+        }
+        list.remove(4);
+        System.out.println("-----------------------------");
+        for (int i = 0; i < list.getSize(); i++) {
+            list.getlist(i);
+        }
+        list.coverNumber(3, 200);
+        System.out.println("-----------------------------");
+        for (int i = 0; i < list.getSize(); i++) {
+            list.getlist(i);
+        }
+        list.clear();
+        System.out.println("-----------------------------");
+        System.out.println(list.getSize());
+        list.getlist(0);
+        list.add(5);
+        list.getlist(0);
+        list.getlist(3);
+    }
+}
